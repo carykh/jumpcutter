65ef7a517f0b1c1ef5ec68471dd93f5a0cd5e4bb
Mister X <martinschulz2000@gmail.com>
2019-04-10 23:52:43 +0200

    fix typo

--- jumpcutter.py
+++ jumpcutter.py
@@ -52,7 +52,7 @@ parser.add_argument('--output_file', '-o', type=str, default=argparse.SUPPRESS, 
 parser.add_argument('--silent_threshold', type=float, default=0.03, help="the volume amount that frames' audio needs to surpass to be consider \"sounded\". It ranges from 0 (silence) to 1 (max volume).")
 parser.add_argument('--sounded_speed', type=float, default=1.00, help="the speed that sounded (spoken) frames should be played at.")
 parser.add_argument('--silent_speed', type=float, default=5.00, help="the speed that silent frames should be played at. 999999 for jumpcutting.")
-parser.add_argument('--frame_margin', type=float, default=1, help="some silent frames adjacent to sounded frames are included to prodive context. How many frames on either the side of speech should be included? That's this variable.")
+parser.add_argument('--frame_margin', type=float, default=1, help="some silent frames adjacent to sounded frames are included to provide context. How many frames on either the side of speech should be included? That's this variable.")
 parser.add_argument('--sample_rate', type=float, default=44100, help="sample rate of the input and output videos.")
 parser.add_argument('--frame_rate', type=float, default=30, help="frame rate of the input and output videos. optional... I try to find it out myself, but it doesn't always work.")
 parser.add_argument('--frame_quality', type=int, default=3, help="quality of frames to be extracted from input video. Range: (1-5).")
