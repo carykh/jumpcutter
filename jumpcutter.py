f4ffb74134452a1db6995a43de1c57b9ef01c9f9
Mikko Vedru <mikko.vedru@gmail.com>
2019-06-02 15:18:08 +0300

    Print default values and put them in one place

--- jumpcutter.py
+++ jumpcutter.py
@@ -12,6 +12,14 @@ import os
 import argparse
 from pytube import YouTube
 
+DEFAULT_SILENT_THRESHOLD = 0.03
+DEFAULT_SOUNDED_SPEED = 1
+DEFAULT_SILENT_SPEED = 5
+DEFAULT_FRAME_MARGIN = 1
+DEFAULT_SAMPLE_RATE = 44100
+DEFAULT_FRAME_RATE = 30
+DEFAULT_FRAME_QUALITY = 3 # 1 is highest, 31 is lowest
+
 def downloadFile(url):
     name = YouTube(url).streams.first().download()
     newname = name.replace(' ','_')
@@ -56,13 +64,13 @@ parser = argparse.ArgumentParser(description='Modifies a video file to play at d
 parser.add_argument('--input_file', type=str,  help='the video file you want modified')
 parser.add_argument('--url', type=str, help='A youtube url to download and process')
 parser.add_argument('--output_file', type=str, default="", help="the output file. (optional. if not included, it'll just modify the input file name)")
-parser.add_argument('--silent_threshold', type=float, default=0.03, help="the volume amount that frames' audio needs to surpass to be consider \"sounded\". It ranges from 0 (silence) to 1 (max volume)")
-parser.add_argument('--sounded_speed', type=float, default=1.00, help="the speed that sounded (spoken) frames should be played at. Typically 1.")
-parser.add_argument('--silent_speed', type=float, default=5.00, help="the speed that silent frames should be played at. 999999 for jumpcutting.")
-parser.add_argument('--frame_margin', type=float, default=1, help="some silent frames adjacent to sounded frames are included to provide context. How many frames on either the side of speech should be included? That's this variable.")
-parser.add_argument('--sample_rate', type=float, default=44100, help="sample rate of the input and output videos")
-parser.add_argument('--frame_rate', type=float, default=30, help="frame rate of the input and output videos. optional... I try to find it out myself, but it doesn't always work.")
-parser.add_argument('--frame_quality', type=int, default=3, help="quality of frames to be extracted from input video. 1 is highest, 31 is lowest, 3 is the default.")
+parser.add_argument('--silent_threshold', type=float, default=DEFAULT_SILENT_THRESHOLD, help="the volume amount that frames' audio needs to surpass to be consider \"sounded\". It ranges from 0 (silence) to 1 (max volume). Default: "+str(DEFAULT_SILENT_THRESHOLD))
+parser.add_argument('--sounded_speed', type=float, default=DEFAULT_SOUNDED_SPEED, help="the speed that sounded (spoken) frames should be played at. Default: "+str(DEFAULT_SOUNDED_SPEED))
+parser.add_argument('--silent_speed', type=float, default=DEFAULT_SILENT_SPEED, help="the speed that silent frames should be played at. 999999 for jumpcutting. Default: "+str(DEFAULT_SILENT_SPEED))
+parser.add_argument('--frame_margin', type=float, default=DEFAULT_FRAME_MARGIN, help="some silent frames adjacent to sounded frames are included to provide context. How many frames on either the side of speech should be included? That's this variable. Default: "+str(DEFAULT_FRAME_MARGIN))
+parser.add_argument('--sample_rate', type=float, default=DEFAULT_SAMPLE_RATE, help="sample rate of the input and output videos. Default: "+str(DEFAULT_SAMPLE_RATE))
+parser.add_argument('--frame_rate', type=float, default=DEFAULT_FRAME_RATE, help="frame rate of the input and output videos. optional... I try to find it out myself, but it doesn't always work. Default: "+str(DEFAULT_FRAME_RATE))
+parser.add_argument('--frame_quality', type=int, default=DEFAULT_FRAME_QUALITY, help="quality of frames to be extracted from input video. 1 is highest, 31 is lowest, 3 is the default. Default: "+str(DEFAULT_FRAME_QUALITY))
 
 args = parser.parse_args()
 
