29af7fa28b034fed0ec07c4b95940f7b1a330429
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-05-16 22:04:58 +0200

    Fail on command fail

--- jumpcutter.py
+++ jumpcutter.py
@@ -96,15 +96,14 @@ AUDIO_FADE_ENVELOPE_SIZE = 400  # smooth out transitiion's audio by quickly fadi
 createPath(TEMP_FOLDER)
 
 command = "ffmpeg -i \"" + INPUT_FILE + "\" -qscale:v " + str(FRAME_QUALITY) + " \"" + TEMP_FOLDER + "/frame%06d.jpg\" -hide_banner"
-subprocess.call(command, shell=True)
+subprocess.run(command, shell=True, check=True)
 
 command = "ffmpeg -i \"" + INPUT_FILE + "\" -ab 160k -ac 2 -ar " + str(SAMPLE_RATE) + " -vn \"" + TEMP_FOLDER + "/audio.wav\""
-
-subprocess.call(command, shell=True)
+subprocess.run(command, shell=True, check=True)
 
 command = "ffmpeg -i \"" + TEMP_FOLDER + "/input.mp4\" 2>&1"
 f = open(TEMP_FOLDER + "/params.txt", "w")
-subprocess.call(command, shell=True, stdout=f)
+subprocess.run(command, shell=True, stdout=f)
 
 sampleRate, audioData = wavfile.read(TEMP_FOLDER + "/audio.wav")
 audioSampleCount = audioData.shape[0]
@@ -198,6 +197,6 @@ for endGap in range(outputFrame,audioFrameCount):
 '''
 
 command = "ffmpeg -framerate " + str(frameRate) + " -i \"" + TEMP_FOLDER + "/newFrame%06d.jpg\" -i " + TEMP_FOLDER + "/audioNew.wav -strict -2 \"" + OUTPUT_FILE + "\""
-subprocess.call(command, shell=True)
+subprocess.run(command, shell=True, check=True)
 
 deletePath(TEMP_FOLDER)
